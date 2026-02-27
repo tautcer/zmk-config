1173e7573b4a5c5258cd043a4e60692e7a790ed8
Tautvydas Cerniauskas <tautvydas.cerniauska@kaluza.com>
2026-02-27 13:56:10 +0000

    POC

--- config/helpers.h
+++ config/helpers.h
@@ -4,43 +4,6 @@
     quick-tap-ms = <220>; \
     hold-trigger-key-positions = <0>;
 
-#define OS_MORPH_IMPL(NAME, ALT_BINDING, DEF_BINDING) \
-    NAME##_d: NAME##_d { \
-        wait-ms = <0>; \
-        tap-ms = <0>; \
-        compatible = "zmk,behavior-macro"; \
-        #binding-cells = <0>; \
-        bindings \
-            = <DEF_BINDING> \
-            ; \
-    }; \
-    NAME##_a: NAME##_a { \
-        wait-ms = <0>; \
-        tap-ms = <0>; \
-        compatible = "zmk,behavior-macro"; \
-        #binding-cells = <0>; \
-        bindings \
-            = <ALT_BINDING> \
-            ; \
-    }; \
-    NAME: NAME { \
-        compatible = "zmk,behavior-layer-morph"; \
-        #binding-cells = <0>; \
-        layers = <ALT_OS>; \
-        bindings \
-            = <&NAME##_d> \
-            , <&NAME##_a> \
-            ; \
-    };
-
-#ifdef HOST_OS
-#define OS_MORPH(NAME, ALT_BINDING, DEF_BINDING) \
-    OS_MORPH_IMPL(NAME, ALT_BINDING, DEF_BINDING)
-#else
-#define OS_MORPH(NAME, ALT_BINDING, DEF_BINDING) \
-    OS_MORPH_IMPL(NAME, DEF_BINDING, ALT_BINDING)
-#endif
-
 #define MACRO(NAME, BINDING) \
     ZMK_MACRO(NAME, \
         wait-ms = <5>; \
@@ -49,5 +12,11 @@
     )
 
 #define TAPHOLD_TP(NAME, TAP, HOLD) \
-    ZMK_HOLD_TAP(NAME, bindings = <HOLD>, <TAP>; MT_CORE)
-
+    ZMK_HOLD_TAP(NAME, \
+      flavor = "tap-preferred"; \
+      #binding-cells = <2>; \
+      tapping-term-ms = <250>; \
+      quick-tap-ms = <175>; \
+      require-prior-idle-ms = <175>; \
+      bindings = <HOLD>, <TAP>; \
+)
